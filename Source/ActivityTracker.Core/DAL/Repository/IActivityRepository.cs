b3450d87ac75f431103f9389772497462d99ad16
ilukianov <ilukianov@gmail.com>
2016-11-02 19:28:18 +0300

    Add Activity domain object and Repository

---
+++ Source/ActivityTracker.Core/DAL/Repository/IActivityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActivityTracker.Core.Domain;
+
+namespace ActivityTracker.Core.DAL.Repository
+{
+    public interface IActivityRepository
+    {
+        void Add(Activity activity);
+    }
+}
